7bd1b10fa5ff3353886fd5f9591a20cf9cb3667f
Clark Farriss <clarkfarriss@Clarks-MBP.attlocal.net>
2021-01-29 21:24:12 -0500

    styles for latest page

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import '../App.css'
 
 const Product = ({product}) => {
     return (
         <div className="card">
             <Link to={`/product/${product.ProductID}`}>
-                <img src={product.PhotoName}></img>
+                <img className="img-component" src={product.PhotoName}></img>
             </Link>
             <div className="card-body">
-            <Link to={`/product/${product.ProductID}`}>
+            <Link to={`/product/${product.ProductID}`} className="card-title-specific">
                 <div className="card-title">
-                    <strong>{product.ItemName}</strong>
+                    <p className="card-title-specific">{product.ItemName}</p>
                 </div>
             </Link>
+            <hr/>
                 <div className="card-text">
-                    <p>${product.BasePrice}</p>
+                    <p className="product-com-price">${product.BasePrice}</p>
                 </div>
             </div>
         </div>
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import items from '../items'
 import Product from '../components/Product'
+import '../App.css'
 
 const HomeScreen = () => {
     return (
         <>
-            <h1>Latest Products</h1>
-            <div className="row">
+            <h1 className="latest-header">Latest Products</h1>
+            <div className="row-hs">
                 {items.map(product => (
                     <div className="col">
                         <Product product={product} />
